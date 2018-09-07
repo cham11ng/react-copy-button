45fbc2f849ca85fa56878c1d6f159b9ee7a4e40c
Sagar Chamling <sgr.raee@gmail.com>
2018-09-07 21:35:15 +0545

    Integrate react copy button library in example

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from 'react-copy-button'
+import { CopyButton } from 'react-copy-button';
 
 export default class App extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+
+    this.imgRef = React.createRef();
+    this.state = {
+      text: '',
+      image: ''
+    };
+  }
+
+  _handleChange = event => this.setState({ [event.target.name]: event.target.value });
+
+  render() {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
+      <main class="wrapper">
+        <section class="container">
+          <div class="row">
+            <div className="column column-80 column-offset-10">
+              <h1># React Copy Button</h1>
+              <input type="text" name="text" placeholder="Type text" onChange={this._handleChange} />
+              <CopyButton className="button button-outline" text={this.state.text}>
+                Copy Text
+              </CopyButton>
+              <hr />
+              <input
+                type="text"
+                name="image"
+                value={this.state.image}
+                placeholder="Paste image url here"
+                onChange={this._handleChange}
+              />
+              <div style={{ textAlign: 'center' }} ref={this.imgRef}>
+                <img
+                  src={this.state.image || 'https://raw.githubusercontent.com/cham11ng/e2n/master/src/assets/logo.png'}
+                  alt="Image"
+                />
+              </div>
+              <CopyButton className="button button-outline" imgRef={this.imgRef}>
+                Copy Image
+              </CopyButton>
+            </div>
+          </div>
+        </section>
+      </main>
+    );
   }
 }
